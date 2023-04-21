7b0732347d39075e0420ab8d30bf9211347e11ed
SOBUE Sho <sho@sobue.name>
2023-04-22 00:06:08 +0900

    Add Bindings

--- consumer/src/main/java/dev/sobue/rabbit/consumer/Application.java
+++ consumer/src/main/java/dev/sobue/rabbit/consumer/Application.java
@@ -1,8 +1,13 @@
 package dev.sobue.rabbit.consumer;
 
+import java.util.function.Consumer;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.messaging.Message;
 
+@Slf4j
 @SpringBootApplication
 public class Application {
 
@@ -10,4 +15,11 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
+	@Bean
+	public Consumer<Message<String>> input() {
+		return m -> {
+			var t = System.currentTimeMillis();
+			log.info("d:{} i:{}", t, m);
+		};
+	}
 }
--- producer/src/main/java/dev/sobue/rabbit/producer/Application.java
+++ producer/src/main/java/dev/sobue/rabbit/producer/Application.java
@@ -1,8 +1,19 @@
 package dev.sobue.rabbit.producer;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.function.StreamBridge;
+import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
+import static org.springframework.util.StringUtils.hasLength;
+
+@RestController
+@RequiredArgsConstructor
 @SpringBootApplication
 public class Application {
 
@@ -10,4 +21,19 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
+	private final StreamBridge streamBridge;
+
+	@PostMapping("/post-message")
+	public ResponseEntity<Void> postMessage(@RequestBody(required = false) String body) {
+		streamBridge.send(
+				"output-out-0",
+				MessageBuilder.withPayload(hasLength(body) ? body : "a").build());
+
+		return ResponseEntity.accepted().build();
+	}
+
+	//@Bean
+	//public Supplier<Message<String>> output() {
+  //		return () -> MessageBuilder.withPayload("a").build();
+	//}
 }
