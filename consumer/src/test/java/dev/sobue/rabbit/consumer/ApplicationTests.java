5ac6769fbd01ac7535fc362f81fffe1984f96469
SOBUE Sho <sho@sobue.name>
2023-04-23 12:27:50 +0900

    Add @AutoConfigureObservability
    https://docs.spring.io/spring-boot/docs/current/reference/html/features.html#features.testing.spring-boot-applications.tracing

--- consumer/src/test/java/dev/sobue/rabbit/consumer/ApplicationTests.java
+++ consumer/src/test/java/dev/sobue/rabbit/consumer/ApplicationTests.java
@@ -1,9 +1,11 @@
 package dev.sobue.rabbit.consumer;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.actuate.observability.AutoConfigureObservability;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@AutoConfigureObservability
 class ApplicationTests {
 
 	@Test
