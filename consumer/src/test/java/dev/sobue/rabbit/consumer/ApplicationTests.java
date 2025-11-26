c66de4ffbda7d93c64398e96ab3bf441210fe464
SOBUE Sho <sho@sobue.dev>
2025-11-26 21:01:18 +0900

    Spring Boot 4.0.0

--- consumer/src/test/java/dev/sobue/rabbit/consumer/ApplicationTests.java
+++ consumer/src/test/java/dev/sobue/rabbit/consumer/ApplicationTests.java
@@ -1,11 +1,13 @@
 package dev.sobue.rabbit.consumer;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.autoconfigure.actuate.observability.AutoConfigureObservability;
+import org.springframework.boot.micrometer.metrics.test.autoconfigure.AutoConfigureMetrics;
+import org.springframework.boot.micrometer.tracing.test.autoconfigure.AutoConfigureTracing;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-@AutoConfigureObservability
+@AutoConfigureMetrics
+@AutoConfigureTracing
 class ApplicationTests {
 
 	@Test
